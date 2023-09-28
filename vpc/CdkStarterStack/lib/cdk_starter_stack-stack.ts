ab7f8a7e01f3423dc7cafca2f60c1af9f98c62c7
Joel Wembo <joelotepawembo@gmail.com>
2023-09-28 23:35:06 +0800

    Add files via upload

---
+++ vpc/CdkStarterStack/lib/cdk_starter_stack-stack.ts
@@ -0,0 +1,78 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStarterStackStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'private-subnet-1',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'public-subnet-1',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'isolated-subnet-1',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
+
+
+      // 👇 update the Name tag for the VPC
+      cdk.Aspects.of(vpc).add(new cdk.Tag('Name', 'my-cdk-vpc'));
+
+      // 👇 update the Name tag for public subnets
+      for (const subnet of vpc.publicSubnets) {
+        cdk.Aspects.of(subnet).add(
+          new cdk.Tag(
+            'Name',
+            `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+              subnet.availabilityZone
+            }`,
+          ),
+        );
+      }
+  
+      // 👇 update the Name tag for private subnets
+      for (const subnet of vpc.privateSubnets) {
+        cdk.Aspects.of(subnet).add(
+          new cdk.Tag(
+            'Name',
+            `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+              subnet.availabilityZone
+            }`,
+          ),
+        );
+      }
+  
+      // 👇 update the Name tag for private subnets
+      for (const subnet of vpc.isolatedSubnets) {
+        cdk.Aspects.of(subnet).add(
+          new cdk.Tag(
+            'Name',
+            `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+              subnet.availabilityZone
+            }`,
+          ),
+        );
+      }
+  
+      new cdk.CfnOutput(this, 'vpcId', {
+        value: vpc.vpcId,
+        description: 'the ID of the VPC',
+      });
+  }
+}
